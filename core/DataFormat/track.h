495dd3b401b8683c1a2afacee3b66bc70a2e1b68
maluethi <matthias.luethi@lhep.unibe.ch>
2018-10-05 12:47:05 +0200

    removed unused variable

--- core/DataFormat/track.h
+++ core/DataFormat/track.h
@@ -50,14 +50,9 @@ namespace larlite {
 				     fFitMomentum(original.fFitMomentum),
 				     fID(original.fID)
     	{	
-		size_t n_offsets = offsets.size();
 		size_t n_steps = original.NumberTrajectoryPoints();
-		
-		//std::cout << "no: " << n_offsets << " ns: " << n_steps << std::endl; 	
-	
 		for (size_t idx=0; idx < n_steps; idx++) {
 			auto p_orig = original.LocationAtPoint(idx);
-			//std::cout << idx << " " << p_orig.X() << " " << p_orig.Y() << " " << p_orig.Z()  << std::endl;
 			fXYZ.push_back(original.LocationAtPoint(idx) + offsets.at(idx));
 		}
 	}
